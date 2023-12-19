86b9c68785e3cd3e8b52bb35ea4c2644720abbb3
Daniel Coello <danielcoellovargas@gmail.com>
2023-12-19 17:07:40 -0500

    T1 - List indexing tuples

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_actions = input("Type add, show or exit: ")
+    user_actions = input("Type add, show, edit or exit: ")
     user_actions = user_actions.strip()
 
     match user_actions:
@@ -12,6 +12,11 @@ while True:
             print(todos)
         case 'exit':
             break
+        case 'edit':
+            number = int(input("Number of the ToDo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new ToDO: ")
+            todos[number] = new_todo
         case _:
             print("Buddy, you entered an unknown command.")
 print('Bye!')
