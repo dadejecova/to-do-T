18b52b58465871f6c961ca947a4ff8215937a39f
Daniel Coello <danielcoellovargas@gmail.com>
2024-01-31 14:51:54 -0500

    T1 - GUI Data input

--- gui.py
+++ gui.py
@@ -1,13 +1,31 @@
 # ToDos Desktop GUI
-
+import time
 import functions
 import PySimpleGUI as Sg
 
 label = Sg.Text("Type in a ToDo")
-input_box = Sg.InputText(tooltip="Enter ToDo")
+input_box = Sg.InputText(tooltip="Enter ToDo", key="ToDo")
 add_button = Sg.Button("Add")
 
-window = Sg.Window('My ToDo App', layout=[[label], [input_box, add_button]])
-window.read()
-print("Hello")
+window = Sg.Window('My ToDo App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 15))
+
+# Just takes the time that the app was executed
+now = time.strftime("%b %d, %Y %H:%M:%S")
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['ToDo'] + " " + now + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case Sg.WINDOW_CLOSED:
+            break
+
+
 window.close()
